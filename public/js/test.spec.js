ea13b0a73fe0c7d9073aa87e14b2607f3fe782b9
a-lysenko <aleksey.lysenko@gmail.com>
2016-08-01 01:34:08 +0300

    - added stylesheet - added test spec file

---
+++ public/js/test.spec.js
@@ -0,0 +1,9 @@
+(function () {
+    'use strict';
+
+    describe('My first test', function () {
+        it('should 4 and 4 to be equal', function () {
+            expect(3+1).toEqual(4);
+        });
+    });
+})();
