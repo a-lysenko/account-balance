039f32bd25e23b892c54f338b4fef29c18641124
a-lysenko <lysa@ciklum.com>
2016-08-02 16:45:00 +0300

    - Fixed structure: added bootstrap in dependencies, added paths to front-end packages into karma conf - Updated structure: removed angular-mocks from bower, removed angular-local-storage from npm dependencies, added prestart

--- karma.conf.js
+++ karma.conf.js
@@ -5,7 +5,10 @@ module.exports = function(config) {
         files: [
             'node_modules/angular/angular.js',
             'node_modules/angular-mocks/angular-mocks.js',
-            'node_modules/angular-local-storage/dist/angular-local-storage.js',
+            'public/libs/angular-local-storage/dist/angular-local-storage.js',
+            'public/libs/angular-animate/angular-animate.js',
+            'public/libs/angular-bootstrap/ui-bootstrap.js',
+            'public/libs/angular-bootstrap/ui-bootstrap-tpls.js',
             'public/components/**/*.module.js',
             'public/components/**/*.js',
             'public/**/*.spec.js'
