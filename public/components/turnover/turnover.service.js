e852befd190f7047c678939a0180781b6505db8c
a-lysenko <aleksey.lysenko@gmail.com>
2016-08-13 03:33:23 +0300

    - updated spec 'create income' in turnover service - implemented 'create income' in turnover service

--- public/components/turnover/turnover.service.js
+++ public/components/turnover/turnover.service.js
@@ -5,12 +5,63 @@
         .factory('turnoverService', turnoverService);
 
     function turnoverService() {
+        var turnoverTypes = {
+            income: 'income',
+            expense: 'expense'
+        };
+        var srcDataTypes = {
+            income: 'income',
+            balance: 'balance'
+        };
+
         return {
             createIncome: createIncome
         };
 
-        function createIncome() {
-            return {}
+        function createIncome(incomeSrc) {
+            return new Income(incomeSrc);
+        }
+
+        function Income(src) {
+            TurnoverItem.call(this, src);
+
+            this.type = turnoverTypes.income;
+            this.srcData.type = srcDataTypes.income;
+            this.aggregate = angular.copy(this.srcData.aggregate);
+        }
+
+        function Expense(src) {
+            TurnoverItem.call(this, src);
+
+            this.type = turnoverTypes.expense;
+            this.srcData.type = srcDataTypes.balance;
+        }
+
+        function TurnoverItem(src) {
+            this.type = null;
+            this.date = Date.now();
+            this.srcData = {
+                type: null,
+                data: angular.copy(src),
+                aggregate: aggregateTurnoverSrcData()
+            };
+            this.aggregate = null;
+            this.spreadByJugs = null;
+            this.balanceByJugs = null;
+            this.iterationBalanceIncrementByJugs = null;
+
+            function aggregateTurnoverSrcData() {
+                var accumulated = {
+                    USD: 0,
+                    UAH: 0
+                };
+                return src.reduce(function (acc, srcItem) {
+                    acc.UAH += srcItem.UAH;
+                    acc.USD += srcItem.USD;
+
+                    return acc;
+                }, accumulated);
+            }
         }
     }
 })();
--- public/components/turnover/turnover.service.spec.js
+++ public/components/turnover/turnover.service.spec.js
@@ -9,15 +9,33 @@ describe('turnover service', function () {
     }));
 
     describe('"createIncome"', function () {
-        var mockIncome = {};
-        var expectedTurnoverIncome = {
-            type: 'income'
+        var mockIncomeItem1 = {source: 'source1', UAH: 12.34, USD: 56.78};
+        var mockIncomeItem2 = {source: 'source2', UAH: 90.12, USD: -34.56};
+        var mockIncome = [mockIncomeItem1, mockIncomeItem2];
+        var mockIncomeAggregate = {
+            UAH: mockIncomeItem1.UAH + mockIncomeItem2.UAH,
+            USD: mockIncomeItem1.USD + mockIncomeItem2.USD
         };
+        var nowDate = {};
+        var incomeType = 'income';
+
+        beforeEach(function () {
+            spyOn(Date, 'now').and.returnValue(nowDate);
+        });
 
         it('should create formatted income', function () {
             var turnoverIncome = turnoverService.createIncome(mockIncome);
+            var srcData = turnoverIncome.srcData;
 
-            expect(turnoverIncome).toEqual(expectedTurnoverIncome);
+            expect(turnoverIncome.type).toBe(incomeType);
+            expect(turnoverIncome.date).toBe(nowDate);
+            expect(srcData.type).toBe(incomeType);
+            expect(srcData.data).toEqual(mockIncome);
+            expect(srcData.aggregate).toEqual(mockIncomeAggregate);
+            expect(turnoverIncome.aggregate).toEqual(mockIncomeAggregate);
+            expect(turnoverIncome.spreadByJugs).toBeNull();
+            expect(turnoverIncome.balanceByJugs).toBeNull();
+            expect(turnoverIncome.iterationBalanceIncrementByJugs).toBeNull();
         });
     });
 
