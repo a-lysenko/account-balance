99fb8d5cab12f207d8937056e04410c3f8b51f2d
a-lysenko <aleksey.lysenko@gmail.com>
2016-08-23 00:17:20 +0300

    - updated bundle-styles task

--- tasks/bundle-styles.js
+++ tasks/bundle-styles.js
@@ -1,30 +1,6 @@
 var nodeSass = require('node-sass');
 var fs = require('fs');
 
-var sassResultCb = function (err, result) {
-    if (err) {
-        console.error('Compiling SCSS -> CSS failed. Reason:', err);
-        return;
-    }
-
-    // TODO - move to config
-    if (!fs.existsSync('./public/css')){
-        fs.mkdirSync('./public/css');
-    }
-
-    fs.writeFile(this.options.outFile, result.css, (err) => {
-        if (err) throw err;
-
-        console.log('Compiling SCSS -> CSS finished. (duration:', result.stats.duration, 'ms)');
-    });
-
-    fs.writeFile(this.options.sourceMap, result.map, (err) => {
-        if (err) throw err;
-
-        console.log('SCSS -> CSS source map created');
-    });
-};
-
 module.exports = (config) => {
     var filePath = [config.src, config.file].join('/');
     var outFilePath = [config.dist, config.outFile].join('/');
@@ -34,4 +10,27 @@ module.exports = (config) => {
         outFile: outFilePath,
         sourceMap: config.sourceMap
     }, sassResultCb);
+
+    function sassResultCb(err, result) {
+        if (err) {
+            console.error('Compiling SCSS -> CSS failed. Reason:', err);
+            return;
+        }
+
+        if (!fs.existsSync(config.dist)){
+            fs.mkdirSync(config.dist);
+        }
+
+        fs.writeFile(this.options.outFile, result.css, (err) => {
+            if (err) throw err;
+
+            console.log('Compiling SCSS -> CSS finished. (duration:', result.stats.duration, 'ms)');
+        });
+
+        fs.writeFile(this.options.sourceMap, result.map, (err) => {
+            if (err) throw err;
+
+            console.log('SCSS -> CSS source map created');
+        });
+    }
 };
