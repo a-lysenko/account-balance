2697c3a6ea929567725f58c9c641579bbeccfff7
a-lysenko <aleksey.lysenko@gmail.com>
2016-12-10 17:51:32 +0200

    - removed unused test

--- src/js/test.spec.js
+++
@@ -1,9 +0,0 @@
-(function () {
-    'use strict';
-
-    describe('My first test', function () {
-        it('should 4 and 4 to be equal', function () {
-            expect(3+1).toEqual(4);
-        });
-    });
-})();
