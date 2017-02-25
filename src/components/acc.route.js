66e19a6f8dc372d417a7656371e6031696f9c49c
a-lysenko <aleksey.lysenko@gmail.com>
2017-02-26 01:26:31 +0200

    - added ui state to tick plan component (draft and basic) - added link to tick plan into tick desk (each item if list) (draft and basic)

--- src/components/acc.route.js
+++ src/components/acc.route.js
@@ -2,7 +2,7 @@
     'use strict';
 
     angular.module('acc')
-        .config(function ($stateProvider, $urlRouterProvider) {
+        .config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
             $urlRouterProvider
                 .otherwise('/');
 
@@ -24,11 +24,22 @@
                     controllerAs: 'vm'
                 })
                 .state('tick-desk', {
-                    url: "/",
+                    url: '/',
                     // TODO - remove balance income directive and use controller here directly
                     // TODO - but previously check if you just can use property 'component'.
                     //        if yes - transform the directive to component and use the approach
                     template: '<tick-desk>Here is tick-desk directive</tick-desk>'
+                })
+                .state('tick-plan', {
+                    url:'/tick-plan',
+                    template: '<tick-plan>Here is tick-plan directive</tick-plan>'
                 });
+
+            // use the HTML5 History API
+
+            //$locationProvider.html5Mode({
+            //    enabled: true,
+            //    requireBase: false
+            //});
         });
 })();
