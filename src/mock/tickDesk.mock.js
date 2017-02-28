be607093680b4044722de3396006c221cb2aa87f
a-lysenko <aleksey.lysenko@gmail.com>
2017-03-01 00:23:20 +0200

    - added mock tick plan data

--- src/mock/tickDesk.mock.js
+++ src/mock/tickDesk.mock.js
@@ -1,8 +1,10 @@
 (function () {
     'use strict';
     angular.module('acc')
-        .run(function (localStorageService, tickDeskDataKey, shortid) {
+        .run(function (localStorageService, shortid,
+                       tickDeskDataKey, tickPlanDataKey) {
             console.info('Mock tickDesk data included in build due to "src/mock/**/*.mock.js" pattern.');
+            console.info('Mock tickPlan data included in build due to "src/mock/**/*.mock.js" pattern.');
 
             localStorageService.set(tickDeskDataKey, [
                 {
@@ -69,5 +71,30 @@
                     ]
                 }
             ]);
+
+            localStorageService.set(tickPlanDataKey, {
+                id: shortid.gen(),
+                plannedValue: 12345.67,
+                spreadValue: 1234.5,
+                unspreadValue: 123.45,
+                spreadPercent: 12.34,
+                unspreadPercent: 56.78,
+                factedPercent: 45,
+                factedId: shortid.gen(),
+                spread: [
+                    {
+                        jugName: 'jug-' + shortid.gen(),
+                        jugDefaultPercent: 51,
+                        jugValue: 4321.05,
+                        jugPercent: 43.21
+                    },
+                    {
+                        jugName: 'jug-' + shortid.gen(),
+                        jugDefaultPercent: 52,
+                        jugValue: 4321.06,
+                        jugPercent: 43.2
+                    }
+                ]
+            })
         });
 })();
