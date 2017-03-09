6b11b110f5542aec89d340176fb65e8ffcfadbb6
a-lysenko <aleksey.lysenko@gmail.com>
2017-03-09 22:17:16 +0200

    - added app GET routing - added request handling based on mock data

--- app/server.js
+++ app/server.js
@@ -2,6 +2,26 @@ const path = require('path');
 const express = require('express');
 const app = express();
 
+const mockDataTickDesk = require('./mock/tickDesk.mock');
+const mockDataTickPlan = require('./mock/tickPlan.mock');
+const mockDataTickFact = require('./mock/tickFact.mock');
+
 app.use(express.static('./public'));
+
+app.get('/tick-desk-data', (req, res) => {
+    res.send(mockDataTickDesk);
+
+});
+app.get('/tick-plan-data/:id', (req, res) => {
+    res.send(mockDataTickPlan);
+
+});
+app.get('/tick-fact-data/:id', (req, res) => {
+    res.send(mockDataTickFact);
+
+});
+
+
 app.listen(8080);
+
 console.log('Server running on port 8080');
