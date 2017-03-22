b2dd95ded1f0b2fe0e4efc39c0c77433db9fabb1
a-lysenko <aleksey.lysenko@gmail.com>
2017-03-22 11:39:15 +0200

    - added jugList mock to app (BE)

---
+++ app/mock/jugList.mock.js
@@ -0,0 +1,19 @@
+const mockJugList = [
+    {
+        id: 'wzpJs7bIv4',
+        name: 'Other',
+        defaultPercent: 15
+    },
+    {
+        id: 'wzqD83hP95',
+        name: 'Charity',
+        defaultPercent: 35
+    },
+    {
+        id: 'wzqGwYQavP',
+        name: 'Entertainment',
+        defaultPercent: 50
+    }
+];
+
+module.exports = mockJugList;
