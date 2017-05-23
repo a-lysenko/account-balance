4a85ab79b5aca40ff19237901f7b96e3d2cd4343
Oleksii Lysenko <olyse@softserveinc.com>
2017-05-23 18:38:24 +0300

    - added schema for spread item - reordered (only visual impact) props in tick schema

--- app/db/tick.model.js
+++ app/db/tick.model.js
@@ -1,23 +1,32 @@
 module.exports = (mongoose) => {
-    //const Schema = mongoose.Schema;
+    const Schema = mongoose.Schema;
 
     const statuses = ['great', 'poor', 'good', ''];
+
+    const spreadItemSchema = new Schema({ 
+        name: String,
+        defaultPercent: Number,
+
+        plannedValue: Number,
+        plannedPercent: Number,
+
+        factedValue: {type: Number, default: 0},
+        factedPercent: {type: Number, default: 0}
+    });
 
     const tickSchema = {
         //id: String,
 
-        plannedDate: {type: Date, default: Date.now},
         plannedValue: Number,
-        factedDate: {type: Date, default: Date.now},
         factedValue: {type: Number, default: 0},
+
         factedPercent: {type: Number, default: 0},
-        spread: [{
-            id: String,
-            plannedValue: Number,
-            plannedPercent: Number,
-            factedValue: {type: Number, default: 0},
-            factedPercent: {type: Number, default: 0}
-        }],
+
+        spread: [spreadItemSchema],
+
+        plannedDate: {type: Date, default: Date.now},
+        factedDate: {type: Date, default: Date.now},
+        
         //tickStatus: {type: String, enum: statuses},
         tickStatus: {type: String, default: ''},
         medals: [/*{
@@ -26,5 +35,5 @@ module.exports = (mongoose) => {
         }...*/]
     };
 
-    return mongoose.model('Tick', tickSchema);
+    return mongoose.model('Tick', tickSchema); // collection name: "ticks" - from name of the model
 };
